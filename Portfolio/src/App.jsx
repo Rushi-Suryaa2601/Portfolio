ea1df519f104ef2bfa523ff3dde5ca0b2e628fde
Rushikesh Suryagandh <rushisuryagandh4455@gmail.com>
2024-03-15 20:52:00 +0530

    commit

---
+++ Portfolio/src/App.jsx
@@ -0,0 +1,13 @@
+
+import Navbar from "./Components/Navbar"
+function App() {
+ return (
+    <>
+    <Navbar/>
+      <div className="  text-7xl text-red-600 font-vibe">Hello</div>
+      
+    </>
+  )
+}
+
+export default App
